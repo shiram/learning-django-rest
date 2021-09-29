a0c33bc59a266baa8ea7f3939ad6ea9af8fe36b5
edward shirambere <edward.shirambere@financetrust.co.ug>
2021-09-29 15:00:57 +0300

    mixin views and generic views to snippet model api

--- quickstart/views.py
+++ quickstart/views.py
@@ -18,6 +18,9 @@ from quickstart.serializers import SnippetSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 
+from rest_framework import mixins
+from rest_framework import generics
+
 #Function views
 @api_view(['GET', 'POST'])
 def snippet_list(request, format=None):
@@ -109,6 +112,41 @@ class SnippetDetail(APIView):
         snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Using Mixins for our views
+
+class SnippetListMixin(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class SnippetDetailMixin(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+# Using Generic class-based views
+
+class SnippetListGeneric(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+class SnippetDetailGeneric(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
